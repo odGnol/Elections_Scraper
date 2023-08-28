b35ad7c83478bb3060685aebfbd1f2863eb6377f
Long Do <64988284+odGnol@users.noreply.github.com>
2023-08-28 23:18:06 +0200

    fce pro vytvoření výstupu

--- vygenerovani_vystupu.py
+++ vygenerovani_vystupu.py
@@ -1,24 +1,10 @@
-# Ve výstupu (soubor .csv) každý řádek obsahuje informace pro konkrétní obec.
-
-# kód obce
-# název obce
-# voliči v seznamu
-# vydané obálky
-# platné hlasy
-# kandidující strany (co sloupec, to počet hlasů pro stranu pro všechny strany).
-
-# codelab
 import csv
-import traceback
 
-
-def zapis_data(data: list, jmeno_souboru: str) -> str:
-    """
-    Zkus zapsat udaje z par. 'data' do souboru formatu .csv.
-    """
+def zapsat_data(data: dict, jmeno_souboru: str) -> str:
     with open(jmeno_souboru, mode="w", encoding="utf-8", newline="") as csv_soubor:
-        sloupce = data[0].keys()
+        sloupce = data.keys()
 
         zapis = csv.DictWriter(csv_soubor, fieldnames=sloupce)
         zapis.writeheader()
-        zapis.writerows(data)
+
+        zapis.writerow(data)
